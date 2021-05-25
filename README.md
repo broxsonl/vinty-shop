0ed8decfd0fd07db87f448e26e25282e4635dfd3
lee.broxson <lee.broxson@avalara.com>
2021-05-24 22:51:57 -0700

    Burning it down. Going to try something else.

--- README.md
+++
@@ -1,1 +0,0 @@
-# vinty-shop
--- server/lib/validate-email-address.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// TODO: Add unit test
-export default function validateEmailAddress(email) {
-  if (!email || typeof email !== 'string') return false;
-
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-};
